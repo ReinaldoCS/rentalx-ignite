1dce9661f74c7a7af5020bc9f65d19b3117ba85d
Reinaldo Correia <santosdoscorreiareinaldo@gmail.com>
2022-06-18 10:39:12 -0300

    Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => response.send('Hello World'));
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () => console.log('Server is listening on port: 3333'));
